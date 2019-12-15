f9b06db6bdc21cfa201dabe4b217a614647ae020
Pius Nyongesa <pinyonge@microsoft.com>
2019-12-15 21:07:17 +0300

    - Disabled other extractors

--- app.py
+++ app.py
@@ -23,17 +23,17 @@ def run(config=None):
     amqp = DundaaAMQPSDK(amqp_url=amqp_url)
 
     # prepare selenium driver
-    options = webdriver.ChromeOptions()
-    options.add_argument("--incognito")
-    options.add_argument("--headless")
-    driver = webdriver.Chrome("chromedriver_win32\chromedriver.exe", options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--incognito")
+    # options.add_argument("--headless")
+    # driver = webdriver.Chrome("chromedriver_win32\chromedriver.exe", options=options)
 
     # load extractors
     ticket_sasa_extractor = ScrapTicketSasa(amqp_client=amqp)
-    mookh_extractor = ScrapMookh(amqp_client=amqp, driver=driver)
+    # mookh_extractor = ScrapMookh(amqp_client=amqp, driver=driver)
     extractors = []
-    extractors.append(mookh_extractor)
-    # extractors.append(ticket_sasa_extractor)
+    # extractors.append(mookh_extractor)
+    extractors.append(ticket_sasa_extractor)
     # run 
     for extractor in extractors:
         run_extractor(extractor)
